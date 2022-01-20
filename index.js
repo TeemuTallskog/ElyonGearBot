cfd6492ef21a48a4a2c709c9c679dc6ad5c862bb
TeemuTallskog <72005659+TeemuTallskog@users.noreply.github.com>
2022-01-20 18:01:31 +0200

    updates

--- index.js
+++ index.js
@@ -1,14 +1,50 @@
 const {Client, Intents}= require("discord.js");
 require('dotenv').config();
 const mongoose = require('./database/mongoose');
-
+const User = require('./models/user');
+const commands = require('./commands/slashcommands');
 const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
 
-client.once('ready', () => {
-    console.log("Bot is online")
+const prefix = "!gear ";
+
+client.on('ready', () => {
+    console.log('Logged in as ${client.user.tag}!')
 });
 
+
+client.on("messageCreate", (message)=> {
+    if(message.content.startsWith(prefix + "update")){
+        let arr = message.content.split(" ");
+        if(!isNaN(arr[2])){
+         User.updateOne({userid: 123},
+             {$set: {'gearscore' : arr[2]}}).catch((err) =>{
+                 console.log(err);
+         });
+            message.reply("you did an update " + message.author.username);
+        }else{
+            message.reply("Invalid input");
+        }
+    }
+})
+
 mongoose.init();
+
+commands.handleReq
+
+const gear = new User({
+    userid: 123,
+    name: "GasGas",
+    gearscore: 777.4,
+    class: "Archer",
+    level: 48
+});
+
+/*gear.save()
+    .then((result) => console.log(result))
+    .catch((err) => console.log(err));
+
+ */
+
 client.login(process.env.TOKEN).then(r => {
     console.log("Successful login");
 });
---
+++ models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    userid: {
+        type: Number,
+        required: true
+    },
+    name:{
+        type: String,
+        required: true
+    },
+    gearscore:{
+        type: Number,
+        required: true
+    },
+    class:{
+        type: String,
+        required: true
+    },
+    level:{
+        type: Number,
+        required: true
+    },
+});
+
+const User = mongoose.model('Usergear', userSchema);
+
+module.exports = User;
