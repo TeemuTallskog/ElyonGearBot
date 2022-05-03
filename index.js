32edb58149c1ffd4a88fb91bf56df6e14ec7594d
TeemuTallskog <72005659+TeemuTallskog@users.noreply.github.com>
2022-05-03 21:06:39 +0300

    Days since update

--- index.js
+++ index.js
@@ -326,17 +326,41 @@ const postList = function (arr, message){
         message.reply("Oops seems like there's no data...");
         return
     }
-    let listStr = "```Name(class): gearscore level";
+    let listStr = "Name(class): gearscore level";
     for(let i = 0; i < arr.length; i++){
         arr[i].lclass = arr[i].lclass.charAt(0).toUpperCase() + arr[i].lclass.substring(1);
         listStr += "\n" + arr[i].name + "(" + arr[i].lclass + "): " + arr[i].gearscore + " " + arr[i].level;
     }
-    listStr += "```";
+    let splitList = listStr.split("\n");
+    let printString = "```css\n" + addDaysSince(splitList, arr).join('\r\n') +  "```";
+
     const embed = new MessageEmbed()
         .setColor('#0099ff')
         .setTitle('List of members sorted by gearscore')
-        .setDescription(listStr);
+        .setDescription(printString);
     message.channel.send({embeds: [embed]});
+}
+
+const addDaysSince = function (arr, gearList){
+    const one_day = 1000 * 60 * 60 * 24;
+    const present = new Date();
+    let longest = arr.reduce(
+        function (a, b) {
+            return a.length > b.length ? a : b;
+        }
+    ).length + 3;
+    let tempArr = arr;
+    tempArr.forEach(function (obj, i) {
+        while(obj.length < longest){
+            obj += " ";
+        }
+        if(i!==0){
+            let res = ((present - new Date(gearList[i - 1].updatedAt)) / (one_day)).toFixed(0);
+            obj += "(" + res + " Days ago)";
+            this[i] = obj;
+        }
+    }, tempArr);
+    return tempArr;
 }
 
 
