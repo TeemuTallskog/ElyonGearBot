1eb0568060fcfa085738c1281e96766bd9057056
TeemuTallskog <72005659+TeemuTallskog@users.noreply.github.com>
2022-05-13 21:35:27 +0300

    list styling

--- index.js
+++ index.js
@@ -326,6 +326,7 @@ const postList = function (arr, message){
         message.reply("Oops seems like there's no data...");
         return
     }
+    /*
     let listStr = "Name(class): gearscore level";
     for(let i = 0; i < arr.length; i++){
         arr[i].lclass = arr[i].lclass.charAt(0).toUpperCase() + arr[i].lclass.substring(1);
@@ -334,6 +335,9 @@ const postList = function (arr, message){
     let splitList = listStr.split("\n");
     let printString = "```css\n" + addDaysSince(splitList, arr).join('\r\n') +  "```";
 
+
+     */
+    let printString = getGearListString(arr);
     const embed = new MessageEmbed()
         .setColor('#0099ff')
         .setTitle('List of members sorted by gearscore')
@@ -362,6 +366,49 @@ const addDaysSince = function (arr, gearList){
     }, tempArr);
     return tempArr;
 }
+
+
+const getGearListString = function(gearList){
+    let col1 = 0;
+    let col2 = 0;
+    //let col3 = 11;
+    //let col4 = 7;
+    const col3 = 5;
+    const col4 = 5;
+    //Name, class, gearscore, level, updated since
+    for(let i = 0; i < gearList.length; i++){
+        if(gearList[i].name.length > col1) col1 = gearList[i].name.length;
+        if(gearList[i].lclass.length > col2) col2 = gearList[i].lclass.length;
+    }
+    return getGrid(gearList, col1+1, col2+2, col3, col4);
+}
+
+const getGrid = function (gearList, col1, col2, col3, col4){
+    let str = [];
+    const present = new Date();
+    const dash = "-";
+    const ldivider = dash.repeat(col1) + "+" + dash.repeat(col2) + "+" + dash.repeat(col3) + "+" + dash.repeat(col4) + "+" +dash.repeat(14);
+    const divider = "";
+    for(let i = 0 ; i < gearList.length; i++){
+            let name = gearList[i].name + getSpaces(col1 - gearList[i].name.length);
+            let lclass = " " + gearList[i].lclass.charAt(0).toUpperCase() + gearList[i].lclass.substring(1) + getSpaces(col2 - gearList[i].lclass.length - 1);
+            let gearscore = " " + gearList[i].gearscore + getSpaces(col3 - 3-1);
+            let level = " " + gearList[i].level + getSpaces(col4 - 2-1);
+            let updatedAt = " " + ((present - new Date(gearList[i].updatedAt)) / (1000 * 60 * 60 * 24)).toFixed(0) + " Days ago\n";
+            str[i] = name + lclass + gearscore + level + updatedAt + divider;
+    }
+    let finalString = "```css\n" + "Name" + getSpaces(col1 - 4) + " Class" + getSpaces(col2 - 6) + " GS" + getSpaces(col3 - 3) + " Lvl " + getSpaces(0) + " Last updated  \n";
+    finalString += ldivider + "\n" + str.join("") + "```";
+    return finalString;
+
+}
+
+const getSpaces = function (i){
+    const space = " ";
+    return space.repeat(i) + "|";
+}
+
+
 
 
 
